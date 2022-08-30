4201cdd09125e023fa63892cd522128a1429b550
Lance <29936745+lancemathias@users.noreply.github.com>
2022-08-30 02:31:54 -0700

    stable slow ver

--- opt.py
+++ opt.py
@@ -1,7 +1,5 @@
 import numpy as np
-import jax.numpy as jnp
-import jax
-from jax.scipy.optimize import minimize
+from scipy.optimize import minimize
 
 def min_fun(mat):
     def pwr_dis(x):
@@ -15,8 +13,8 @@ def min_fun(mat):
 
 def better_obj(mat):
     def pwr_dis(x):
-        xs = jnp.repeat(x[:,None], x.shape, 1)
-        return jnp.sum((xs > xs.T).astype(int)*(xs - xs.T)**2*mat)
+        xs = np.repeat(x[:,None], x.shape, 1)
+        return np.sum((xs > xs.T).astype(int)*(xs - xs.T)**2*mat)
     return pwr_dis
 
 def solve(mat, bounds):
@@ -24,44 +22,38 @@ def solve(mat, bounds):
     x0 = np.random.random(bounds.shape[0])
     return minimize(obj, x0, bounds=bounds)
 
-@jax.jit
-def jax_solve(mat, clamp_inds, clamp_vals):
-    n = mat.shape[0]
-    def obj(x):
-        xs = jnp.repeat(x[:n,None], n, 1)
-        lag = x[clamp_inds+n] - clamp_vals
-        return jnp.sum((xs > xs.T).astype(int)*(xs - xs.T)**2*mat) + jnp.dot(lag, x[n:])
-    x0 = np.random.random(n + clamp_inds.size)
-    return minimize(obj, x0, method='BFGS')
-
 def train(mat, xs, ys, node_cfg, epochs, gamma = 0.01, nu = 0.1, ic = None):
-    assert jnp.count_nonzero(node_cfg > 0) == xs.shape[1], "Shape of training examples doesn't match number of inputs."
-    assert jnp.count_nonzero(node_cfg < 0) == ys.shape[1], "Shape of training examples doesn't match number of outputs."
+    assert np.count_nonzero(node_cfg > 0) == xs.shape[1], "Shape of training examples doesn't match number of inputs."
+    assert np.count_nonzero(node_cfg < 0) == ys.shape[1], "Shape of training examples doesn't match number of outputs."
 
-    trainable_mask = jnp.logical_and(mat == mat.T, mat > 0)
+    trainable_mask = np.logical_and(mat == mat.T, mat > 0)
     ic = ic or np.random.rand(*node_cfg.shape)
     n = ic.shape[0]
 
-    # bounds = jnp.array([[None, None] for _ in node_cfg])
-    # bounds[-1,: ] = [0, 0] # ground node
+    bounds = np.array([[None, None] for _ in node_cfg])
+    bounds[-1,: ] = [0, 0] # ground node
 
-    in_nodes = jnp.argwhere(node_cfg > 0).flatten()
-    out_nodes = jnp.argwhere(node_cfg < 0).flatten()
-    all_nodes = jnp.concatenate((in_nodes, out_nodes))
+    in_nodes = np.argwhere(node_cfg > 0).flatten()
+    out_nodes = np.argwhere(node_cfg < 0).flatten()
 
     for _ in range(epochs):
         for x, y in zip(xs, ys):
                 # Solve free state
-                free = jax_solve(mat, in_nodes, x)
+                bounds[in_nodes, 0] = x
+                bounds[in_nodes, 1] = x
+                bounds[out_nodes, 0] = None
+                bounds[out_nodes, 1] = None
+                free = solve(mat, bounds)
                 # Solve clamped state
-                clamps = y * nu + (1-nu) * free.x[out_nodes]
-                clamped = jax_solve(mat, all_nodes, jnp.concatenate((x, clamps)))
+                bounds[out_nodes, 0] = y * nu + (1-nu) * free.x[out_nodes]
+                bounds[out_nodes, 1] = y * nu + (1-nu) * free.x[out_nodes]
+                clamped = solve(mat, bounds)
 
                 if not (free.success and clamped.success):
-                    return free
+                    return free.message + ',' + clamped.message
 
-                free_rep = jnp.repeat(free.x[:,None], n, axis=1)
-                clamped_rep = jnp.repeat(clamped.x[:,None], n, axis=1)
+                free_rep = np.repeat(free.x[:,None], n, axis=1)
+                clamped_rep = np.repeat(clamped.x[:,None], n, axis=1)
 
                 delta_free = free_rep - free_rep.T
                 delta_clamped = clamped_rep - clamped_rep.T
@@ -73,13 +65,13 @@ def train(mat, xs, ys, node_cfg, epochs, gamma = 0.01, nu = 0.1, ic = None):
 
 def inference(mat, xs, node_cfg):
     n = xs.shape[0]
-    bounds = jnp.array([[None, None] for _ in node_cfg])
+    bounds = np.array([[None, None] for _ in node_cfg])
     bounds[-1,: ] = [0, 0] # ground node
 
-    in_nodes = jnp.argwhere(node_cfg > 0).flatten()
+    in_nodes = np.argwhere(node_cfg > 0).flatten()
     out_mask = node_cfg < 0
 
-    outputs = jnp.empty(shape=(n, out_mask.size))
+    outputs = np.empty(shape=(n, out_mask.size))
 
     for i in range(n):
         bounds[in_nodes, 0] = xs[i]
