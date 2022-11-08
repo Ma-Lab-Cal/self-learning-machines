7f6bc42c395e20cbb30451be747121f8e7ab7e2b
Lance <29936745+lancemathias@users.noreply.github.com>
2022-11-08 14:20:33 -0800

    checkpoint before rework

--- spice_net.py
+++ spice_net.py
@@ -23,30 +23,32 @@ class LinearNetwork(Circuit):
 
     def solve(self, inputs, output_clamps = None):
         circ = self
-        in_nodes, all_nodes= np.sum(self.node_cfg > 0), np.sum(self.node_cfg != 0)
+        in_nodes, out_nodes = self.node_cfg
+        all_nodes = np.concatenate((in_nodes, out_nodes))
         if output_clamps is not None:
             inputs = np.hstack((inputs, output_clamps))
-        if inputs.size == in_nodes:
-            node_inds = np.argwhere(self.node_cfg > 0).flatten()
-        elif inputs.size == all_nodes:
-            node_inds = np.concatenate((np.argwhere(self.node_cfg > 0).flatten(), np.argwhere(self.node_cfg < 0).flatten()))
+        if inputs.size == in_nodes.shape[0]:
+            node_inds = in_nodes
+        elif inputs.size == all_nodes.shape[0]:
+            node_inds = all_nodes
         else:
-            raise ValueError(f'Expected {all_nodes if output_clamps is not None else in_nodes} nodes but got {inputs.size}')
-        for v, ind in zip(inputs.flatten(), node_inds):
-            circ.V(ind, circ.__nodes__[ind], circ.gnd, v)
+            raise ValueError(f'Expected {all_nodes.shape[0] if output_clamps is not None else in_nodes.shape[0]} nodes but got {inputs.size}')
+        for i, v, ind in zip(range(inputs.size), inputs.flatten(), node_inds):
+            circ.V(i+1, circ.__nodes__[ind[0]], circ.__nodes__[ind[1]], v)
         netlist = str(circ)
         with open(f'{self.name}.cir', 'wt') as f:
             f.write(netlist + '.op\n')
         system(f'/Applications/LTspice.app/Contents/MacOS/LTspice -b {self.name}.cir')
-        for i in node_inds:
-            circ[f'V{i}'].detach()
+        for i in range(inputs.size):
+            circ[f'V{i+1}'].detach()
 
         l = ltspice.Ltspice(f'{self.name}.raw')
         l.parse()
-        return np.concatenate([l.get_data(f'V({i})') or [0] for i in self.__nodes__])
+        return np.concatenate([l.get_data(f'V({i+1})') or [0] for i in self.__nodes__])
 
     def predict(self, inputs):
-        return self.solve(inputs)[self.node_cfg < 0]
+        sol = self.solve(inputs)
+        return sol[self.node_cfg[1].T[1]] - sol[self.node_cfg[1].T[0]]
 
 class NonLinearNetwork(LinearNetwork):
     def __init__(self, name, con_mat, node_cfg):
