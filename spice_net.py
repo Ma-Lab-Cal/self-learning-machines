1ae04240d219c4b040bf2fc92dbf8a73145cb510
Lance <29936745+lancemathias@users.noreply.github.com>
2022-11-11 20:04:35 -0800

    working linear regression

--- spice_net.py
+++ spice_net.py
@@ -6,60 +6,84 @@ from os import system
 
 class LinearNetwork(Circuit):
     def __init__(self, name, con_mat, node_cfg, epsilon=1e-9):
+        assert (con_mat == con_mat.T).all(), 'connectivity matrix is not symmetric!'
         self.name = name
+        self.epsilon = epsilon
         super().__init__(name)
         self.__nodes__ = np.array([i for i in range(con_mat.shape[0])])
-        self.con_mat = con_mat
-        self.node_cfg = node_cfg
+        self.inputs = [self.V(n+1, *inds) \
+         for n, inds in enumerate(node_cfg[0])]
+        self.outputs = [self.V(n+1 + len(self.inputs), *inds) \
+         for n, inds in enumerate(node_cfg[1])]
+        for source in self.inputs:
+            source.enabled = False
+        for source in self.outputs:
+            source.enabled = False
+
         res_mat = np.minimum(np.triu(con_mat, k=1), con_mat.T) > 0
-        self.epsilon = epsilon
+        self.edges = [self.R(n+1, *coord, con_mat[coord[0], coord[1]]) \
+        for n, coord in enumerate(np.argwhere(res_mat > 0))]
 
-        for i, coord in enumerate(np.argwhere(res_mat > 0)):
-            self.R(i, self.__nodes__[coord[0]], self.__nodes__[coord[1]], 1/con_mat[coord[0], coord[1]])
+    def update_r(self, updates):
+        '''updates internal resistances given a list of resistance deltas'''
+        assert len(self.edges) == len(updates), \
+            f'Have {len(self.edges)} resistors but {len(updates)} updates'
+        for R, delta in zip(self.edges, updates):
+            R.resistance = min(R.resistance - delta, self.epsilon)
 
-    def update(self, updates):
-        for i, n in enumerate(updates):
-            self[f'R{i}'].resistance = max(1./(1./self[f'R{i}'].resistance + n), self.epsilon)
+    def update_y(self, updates):
+        '''updates internal resistances given a list of admittance deltas'''
+        assert len(self.edges) == len(updates), \
+            f'Have {len(self.edges)} resistors but {len(updates)} updates'
+        for R, delta in zip(self.edges, updates):
+            R.resistance = 1./max(1./R.resistance + delta, self.epsilon)
 
-    def solve(self, inputs, output_clamps = None):
+    def _solve(self, inputs, outputs = None):
         circ = self
-        in_nodes, out_nodes = self.node_cfg
-        all_nodes = np.concatenate((in_nodes, out_nodes))
-        if output_clamps is not None:
-            inputs = np.hstack((inputs, output_clamps))
-        if inputs.size == in_nodes.shape[0]:
-            node_inds = in_nodes
-        elif inputs.size == all_nodes.shape[0]:
-            node_inds = all_nodes
+        assert len(inputs) == len(self.inputs), \
+            f'Expected {len(self.inputs)} but got {len(inputs)} inputs'
+
+        for source, V in zip(self.inputs, inputs):
+            source.enabled = True
+            source.dc_value = V
+        
+        if outputs is not None:
+            assert len(outputs) == len(self.outputs), \
+                f'Expected {len(self.outputs)} but got {len(outputs)} inputs'
+
+            for source, V in zip(self.outputs, outputs):
+                source.enabled = True
+                source.dc_value = V
+
         else:
-            raise ValueError(f'Expected {all_nodes.shape[0] if output_clamps is not None else in_nodes.shape[0]} nodes but got {inputs.size}')
-        for i, v, ind in zip(range(inputs.size), inputs.flatten(), node_inds):
-            circ.V(i+1, circ.__nodes__[ind[0]], circ.__nodes__[ind[1]], v)
+            for source in self.outputs:
+                source.enabled = False
+
         netlist = str(circ)
         with open(f'{self.name}.cir', 'wt') as f:
             f.write(netlist + '.op\n')
         system(f'/Applications/LTspice.app/Contents/MacOS/LTspice -b {self.name}.cir')
-        for i in range(inputs.size):
-            circ[f'V{i+1}'].detach()
-
+        
         l = ltspice.Ltspice(f'{self.name}.raw')
         l.parse()
-        return np.concatenate([l.get_data(f'V({i+1})') or [0] for i in self.__nodes__])
+        return l
+
+    def solve(self, inputs, outputs = None):
+        l = self._solve(inputs, outputs)
+        return np.concatenate([l.get_data(f'V({i})') or [0] for i in self.__nodes__])
 
     def predict(self, inputs):
-        sol = self.solve(inputs)
-        return sol[self.node_cfg[1].T[1]] - sol[self.node_cfg[1].T[0]]
+        l = self._solve(inputs)
+        out = np.zeros(len(self.outputs))
+        for i, v in enumerate(self.outputs):
+            a, b = v.node_names
+            v1 = l.get_data(f'V({a})') or [0]
+            v2 = l.get_data(f'V({b})') or [0]
+            out[i] = v1[0] - v2[0]
+        return out
 
 class NonLinearNetwork(LinearNetwork):
     def __init__(self, name, con_mat, node_cfg):
         super().__init__(name, con_mat, node_cfg)
-
-    def solve(self, inputs):
-        assert inputs.size == np.sum(self.node_cfg > 0), 'invalid input size'
-        circ = self.clone()
-        for v, ind in zip(inputs.flatten(), np.argwhere(self.node_cfg > 0).flatten()):
-            circ.V(ind, circ.__nodes__[ind], circuit.gnd, v)
-        netlist = str(circ)
-        with open(f'{self.name}.cir', 'wt') as f:
-            f.write(netlist + '.op\n')
-        system(f'/Applications/LTspice.app/Contents/MacOS/LTspice -b {self.name}.cir')
+        # TODO: different constructor which allows for nonlinear components
+        # everything else should be the same as in the linear case
