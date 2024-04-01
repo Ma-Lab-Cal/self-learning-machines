a753c78782908ce121ff0e8ed4e70c0db488d93d
Lance <29936745+lancemathias@users.noreply.github.com>
2024-04-01 00:12:07 -0700

    before dataset sweep

--- transistor_sweep.sh
+++ transistor_sweep.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Sweep over learning rates from 1e-3 to 1e-1
-python train_nets.py net xor source_reference --learning_rate=1e-1 --num_iterations=100000 --num_checkpoints=20
-python train_nets.py net xor source_reference --learning_rate=1e-2 --num_iterations=100000 --num_checkpoints=20
-python train_nets.py net xor source_reference --learning_rate=1e-3 --num_iterations=100000 --num_checkpoints=20
+iterations=100000
 
-python train_nets.py net xor ground_reference --learning_rate=1e-1 --num_iterations=100000 --num_checkpoints=20
-python train_nets.py net xor ground_reference --learning_rate=1e-2 --num_iterations=100000 --num_checkpoints=20
-python train_nets.py net xor ground_reference --learning_rate=1e-3 --num_iterations=100000 --num_checkpoints=20
+# Sweep over learning rates from 1e-3 to 1e-1 (finish sweep)
+python train_nets.py net xor ground_reference --learning_rate=1e-1 --num_iterations=$iterations --num_checkpoints=20
+python train_nets.py net xor ground_reference --learning_rate=1e-2 --num_iterations=$iterations --num_checkpoints=20
+python train_nets.py net xor ground_reference --learning_rate=1e-3 --num_iterations=$iterations --num_checkpoints=20
+
+# Try new run with different constants to test robustness
+python train_nets.py no_scale xor source_reference --learning_rate=1e-1 --num_iterations=$iterations --num_checkpoints=20 --dataset=data/xor_train_data_no_scale.npz
+python train_nets.py no_scale xor source_reference --learning_rate=1e-2 --num_iterations=$iterations --num_checkpoints=20 --dataset=data/xor_train_data_no_scale.npz
+python train_nets.py no_scale xor source_reference --learning_rate=1e-3 --num_iterations=$iterations --num_checkpoints=20 --dataset=data/xor_train_data_no_scale.npz
+
+python train_nets.py no_scale xor ground_reference --learning_rate=1e-1 --num_iterations=$iterations --num_checkpoints=20 --dataset=data/xor_train_data_no_scale.npz
+python train_nets.py no_scale xor ground_reference --learning_rate=1e-2 --num_iterations=$iterations --num_checkpoints=20 --dataset=data/xor_train_data_no_scale.npz
+python train_nets.py no_scale xor ground_reference --learning_rate=1e-3 --num_iterations=$iterations --num_checkpoints=20 --dataset=data/xor_train_data_no_scale.npz
